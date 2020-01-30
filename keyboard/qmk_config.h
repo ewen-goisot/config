f15a6bacb6ed27fd3d6438c5396d0f166d80a72d
ewen-goisot <ewen.goisot@gmail.com>
2020-01-30 04:29:02 +0100

    Create qmk_config.h
    qmk firmware for Ergodox

---
+++ keyboard/qmk_config.h
@@ -0,0 +1,31 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#undef DEBOUNCE
+#define DEBOUNCE 40
+
+#undef ONESHOT_TIMEOUT
+#define ONESHOT_TIMEOUT 2500
+
+#define PERMISSIVE_HOLD
+
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL 12
+
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY 40
+
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED 5
+
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX 40
+
+#undef MOUSEKEY_WHEEL_MAX_SPEED
+#define MOUSEKEY_WHEEL_MAX_SPEED 5
+
+#undef MOUSEKEY_WHEEL_TIME_TO_MAX
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 80
+
