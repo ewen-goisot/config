3668182685d3009aa129612ba5d2ba8df9674f31
ewen-goisot <ewen.goisot@gmail.com>
2020-02-03 04:25:43 +0100

    "file" infos, video formats
    mediainfo needs to be installed too

--- pv.sh
+++ pv.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo "\e[1;033m$(file -b $1)\e[0m"
 case "$1" in
 	*.tar*) tar tf "$1";;
 	*.zip) unzip -l "$1";;
@@ -7,7 +8,8 @@ case "$1" in
 	*.7z) 7z l "$1";;
 	*.pdf) pdftotext "$1" -;;
 	*.odt|*.ods|*.odp|*.sxw) odt2txt "$1";;
-	*.png|*.jpg|*.jpeg|*.JPG) img2txt -f utf8 -d none -W 120 "$1";;
+	*.png|*.jpg|*.jpeg|*.JPG) img2txt -f utf8 -d none -W 80 "$1";;
+	*.mp4|*.webm|*.mkv) mediainfo "$1";;
 	*.x|*.out|*.bin) xxd "$1";;
 	*) highlight -O ansi "$1" || cat "$1";;
 esac
