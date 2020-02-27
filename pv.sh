7c176606e8900f1302118399efbaa52ec991e514
ewen-goisot <ewen.goisot@gmail.com>
2020-02-27 11:42:01 +0100

    more audio formats (.avi .mov …)

--- pv.sh
+++ pv.sh
@@ -9,8 +9,10 @@ case "$1" in
 	*.pdf) pdftotext "$1" -;;
 	*.odt|*.ods|*.odp|*.sxw) odt2txt "$1";;
 	*.png|*.jpg|*.jpeg|*.JPG) img2txt -f utf8 -d none -W 80 "$1";;
-	*.mp3|*.opus|*.m4a|*.aac|*.wav|*.mp4|*.webm|*.mkv) mediainfo "$1";;
+	*.mp3|*.opus|*.m4a|*.aac|*.wav|*.mp4|*.avi|*.webm|*.mkv|*.wma|*.mov) mediainfo "$1";;
+	*.iso|*.MTS)echo "other";;
 	*.x|*.out|*.bin) xxd "$1";;
+	#*.txt|*.c|*.cpp|*.h|*.hpp|*.sh)echo "\e[1;033m$(wc $1 | awk '{print $1 "l " $2 "w " $3 "c"}')\e[0m";
 	*)echo "\e[1;033m$(wc $1 | awk '{print $1 "l " $2 "w " $3 "c"}')\e[0m";
 		highlight -O ansi "$1" 2>/dev/null || cat "$1";;
 esac
